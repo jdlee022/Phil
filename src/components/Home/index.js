b69d87df37919b7993968591988f267f59d980e5
Sean Naylor <Sean Naylor>
2017-06-28 19:33:18 -0700

    ongoing css construction

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 
 import './style.css';
 import teaser from './teaser.png';
+import image1 from './images/IMG_1693.JPG';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <div className="col-md-6 col-md-offset-3 text-center">
-                    <img src={teaser} alt="teaser" className="teaser" />
+                <div className="container">
+                    <img src={image1} alt="teaser" className="teaser" />
                 </div>
             </div>
         );
