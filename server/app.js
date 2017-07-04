edcd87c611e3deb7a7731a3a1e449f3f90075ad1
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-07-03 22:25:22 -0700

    change heroku database

--- server/app.js
+++ server/app.js
@@ -14,9 +14,10 @@ const mongo = require('mongodb');
 const mongoose = require('mongoose');
 var Promise = require("bluebird");
 mongoose.Promise = Promise;
-//heroku db: 'mongodb://heroku_vrds24zc:n3skg2tmaquasli0cs0ta4elrq@ds139322.mlab.com:39322/heroku_vrds24zc'
+//heroku db: 'mongodb://heroku_5g1vn335:h5blucfg46r4t8a8nk8iv0qcvg@ds147979.mlab.com:47979/heroku_5g1vn335'
 //local db: 'mongodb://localhost:27017/loginapp'
-mongoose.connect('mongodb://heroku_vrds24zc:n3skg2tmaquasli0cs0ta4elrq@ds139322.mlab.com:39322/heroku_vrds24zc');
+//mongo ds147979.mlab.com:47979/heroku_5g1vn335 -u <dbuser> -p <dbpassword>
+mongoose.connect('mongodb://heroku_5g1vn335:h5blucfg46r4t8a8nk8iv0qcvg@ds147979.mlab.com:47979/heroku_5g1vn335');
 const db = mongoose.connection;
 
 const app = express();
