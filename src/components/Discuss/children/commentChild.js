eefa2b4b020b3373421370d1727d0ef0298e8797
Jon Lee <jdlee022@gmail.com>
2017-07-07 15:13:40 -0700

    changed dashboard to topics

--- src/components/Discuss/children/commentChild.js
+++ src/components/Discuss/children/commentChild.js
@@ -19,22 +19,27 @@ export default class CommentChild extends Component {
 
     // Set state with result from API after component mounts
     componentDidMount() {
+
         API.getUserById(this.props.data.user).then((response) => {
-            this.setState({
-                username: response.data.username,
-                userScore: response.data.highScore
-            })
+            if (response.data) {
+                this.setState({
+                    username: response.data.username,
+                    userScore: response.data.highScore
+                })
+            }
         });
+
+
     }
 
     render() {
 
         return (
-            <li className="row" style={{margin: '0px'}}>
+            <li className="row" style={{ margin: '0px' }}>
                 <div className="col-xs-12 comment-list-element">
                     <h2>By <span className="username">{this.state.username}</span> on {this.state.date}</h2>
                     <h3>{this.state.username}'s high score: {this.state.userScore}</h3>
-                    <h4 style={{whiteSpace: 'pre-wrap'}}>{this.state.text}</h4>
+                    <h4 style={{ whiteSpace: 'pre-wrap' }}>{this.state.text}</h4>
                 </div>
             </li>
         );
